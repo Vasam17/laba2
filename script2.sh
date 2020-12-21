a603d24c623b17264c3ac0a381ad477259454f81
ammvsu <dilshodbek2k17@yandex.ru>
2020-12-21 12:55:40 +0300

    first commit

---
+++ script2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/
+mkdir laba1
+cd /home/laba1
+cp /root/.ash_history  /home/laba1
+mv .ash_history result2.ash_history.bak
+cp /root/.ash_history  /home/laba1
+mv .ash_history second.ash_history
+
+
